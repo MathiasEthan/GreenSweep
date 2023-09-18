cc74eec5d36418aa8ed1acf5d32cb8b5b1a0a5de
Ethan Mathias <54030176+MathiasEthan@users.noreply.github.com>
2023-09-18 10:02:36 +0530

    please let this work

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,44 +13,48 @@ final passwordController = TextEditingController();
       backgroundColor: const Color(0xFFF7F0F0),
       body: SafeArea(
         child: Center(
-          child: Column(children: [
-
-           const SizedBox(height: 100),
-            //logo
-            const Icon(Icons.emoji_emotions_outlined, size: 100, color: Colors.black),
-             const SizedBox(height: 35),
-            //welcome back
-           const Text('Welcome Back', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFF011B10))),
-           const SizedBox(height: 35),
-            //email
-            MyTextField(
-              controller: usernameController,
-              hintText: 'Email',
-              obscureText: false,
-            ),
-              const SizedBox(height: 10),
-            //password
-            MyTextField(
-              controller: passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-  
-  
-            //forgot password
-            const SizedBox(height: 5),
-            const Text('Forgot Password?',
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Color(0xFF011B10)),
-            ),
-            //login button
-              const SizedBox(height: 20),
-              MyButton(),
-            //or continue with
-              
-            //google + apple sign in buttons
-              
-            //not a member? sign up
-          ]),
+          child: Padding(
+            padding: const EdgeInsets.all(1.0),
+            
+            child: Column(children: [
+          
+             const SizedBox(height: 20),
+              //logo
+               Image.asset('assets/images/greensweep.png', scale: 1.0,),
+               
+              //welcome back
+             const Text('Welcome Back', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: Color(0xFF133A1B))),
+             const SizedBox(height: 5),
+              //email
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+                const SizedBox(height: 10),
+              //password
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+            
+            
+              //forgot password
+              const SizedBox(height: 5),
+              const Text('Forgot Password?',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Color(0xFF011B10)),
+              ),
+              //login button
+                const SizedBox(height: 20),
+                MyButton(),
+              //or continue with
+                
+              //google + apple sign in buttons
+                
+              //not a member? sign up
+            ]),
+          ),
         ),
       ),
     );
